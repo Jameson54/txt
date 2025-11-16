539431dd2711e8ff804b13c4c50cd7ed099442f5
Jameson54 <hehlol123123@mail.ru>
2025-11-16 20:49:34 +0700

    Update txt.cpp

--- txt.cpp
+++ txt.cpp
@@ -2,7 +2,7 @@
  * Автор: Кондратенко Павел    *
  *                             *
  *******************************/
- 
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -15,34 +15,37 @@ int main() {
   cout << "Не удалось открыть файл." << endl;
   return 1;
   }
-    
+
   string line;
   getline(fin, line);
-    
+
   fin.close();
-    
-    
-  for (size_t i = 0; i < line.length(); i++) {
-    char current_char = line[i];
+
+
+  for (size_t СurrentVariableFirst = 0; СurrentVariableFirst < line.length(); ++СurrentVariableFirst) {
+    char current_char = line[СurrentVariableFirst];
     bool already_counted = false;
-        
-  for (size_t j = 0; j < i; j++) {
-    if (line[j] == current_char) {
+
+  for (size_t СurrentVariableTwo = 0; СurrentVariableTwo < СurrentVariableFirst; ++СurrentVariableTwo) {
+    if (line[СurrentVariableTwo] == current_char) {
     already_counted = true;
     break;
     }
   }
-        
+
     if (!already_counted) {
         int count = 0;
-        for (size_t k = 0; k < line.length(); k++) {
-            if (line[k] == current_char) {
+        for (size_t СurrentVariableThree = 0; СurrentVariableThree < line.length(); ++СurrentVariableThree) {
+            if (line[СurrentVariableThree] == current_char) {
                 count++;
             }
         }
 
             cout << current_char << " встречается " << count << " раз(а)" << endl;
     }
+}
+
+    return 0;
 }
     
     return 0;
